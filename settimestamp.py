43025081574ce65d54dfffb35dc3a310248b3868
Sabrina Liao <sabrina.g.liao@gmail.com>
2017-01-05 22:40:52 -0800

    Add useless [for us] function for getting creation date of a file. Include commented out code for setting last modified timestamp for a file.

--- settimestamp.py
+++ settimestamp.py
@@ -1,13 +1,38 @@
-import sys
+import sys, os, platform, time
+from datetime import datetime
 
-def _set_timestamp():
-    return print(sys.argv[1]) #filename
+def _get_creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
 
+def _set_timestamp(path):
+    creation_date = _get_creation_date(path) # not needed; just used to see what format the timestamp gets returned in
+    print(time.time()) # to get now time
+    # if path == 'README.md':
+    #     # creation_date in this example on my linux is actually last-modified time in linux
+    #     os.utime(path, (os.stat(path).st_atime, creation_date))
+    return print(creation_date)
+
+# STARTS HERE
 # Check if any arguments beside this python file are given
 if len(sys.argv) <= 1:
     print('Need to pass argument')
 else:
     # Retrieve file/filename from input/argument (TODO: also accept folder name eventually so can change multiple photos at once)
+    # ^-- read: https://docs.python.org/3/tutorial/inputoutput.html
     # If folder, only grab .jpg(?) files (mainly want to avoid the CR2 files from echow)
-    _set_timestamp()
+    _set_timestamp(sys.argv[1])
 
