caef4e3f63e347704c816701b94d3903ce43b21a
sabliao <sabrinagliao@gmail.com>
2017-01-07 23:50:28 -0800

    Committed knowing there's an error w/ struct_time. Figuring out how to get epoch time from datetime components.

--- settimestamp.py
+++ settimestamp.py
@@ -18,22 +18,21 @@ def _get_creation_date(path_to_file):
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
-def _set_timestamp(path):
+def _set_timestamp(filename):
     # check for file pattern
-    jason_pattern= re.compile("201611(2\d)_(\d{2,2})(\d{2,2})(\d{2,2})\.jpg") # jason's file pattern
-    if jason_pattern.match(path):
+    jason_pattern= re.compile("(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})\.jpg") # jason's file pattern
+    match = jason_pattern.match(filename)
+    if match:
         # It's a pic by Jason
-        print(path)
-        # Get the day (1st capture group), hour (2nd capture group), minute (3rd capture group), and millisecond (4th capture group)
-        # and turn into unix epoch time to set on the last modified datetime field
-    creation_date = _get_creation_date(path) # not needed; just used to see what format the timestamp gets returned in
+        # Turn components of date into epoch time to set on the last modified datetime field
+        # time.struct_time takes: year, month, day, hr, min, sec, weekday, day in year, is DST
+        struct = time.struct_time(tm_year=match.group(1), tm_mon=match.group(2), tm_mday=match.group(3),
+            tm_hour=match.group(4), tm_min=match.group(5), tm_sec=match.group(6), tm_isdst=0)
+        print(time.mktime(struct))
+    # creation_date = _get_creation_date(path) # not needed; just used to see what format the timestamp gets returned in
     # print(time.time()) # to get now time
     # print time.strftime("Before: %m/%d/%Y %I:%M:%S %p",time.localtime(os.stat(path).st_atime))
-    # if path == 'README.md':
-    #     # creation_date in this example on my linux is actually last-modified time in linux
-    #     os.utime(path, (os.stat(path).st_atime, creation_date))
-    #     print time.strftime("After: %m/%d/%Y %I:%M:%S %p",time.localtime(os.stat(path).st_atime))
-    print(creation_date)
+    # os.utime(path, (os.stat(path).st_atime, creation_date))
     return
 
 # STARTS HERE
@@ -44,5 +43,11 @@ else:
     # Retrieve file/filename from input/argument (TODO: also accept folder name eventually so can change multiple photos at once)
     # ^-- read: https://docs.python.org/3/tutorial/inputoutput.html
     # If folder, only grab .jpg(?) files (mainly want to avoid the CR2 files from echow)
-    _set_timestamp(sys.argv[1])
+    path = sys.argv[1]
+    has_folder_in_path = re.compile("^.*\/*.*\/(.*\..*)$").match(path)
+    if has_folder_in_path:
+        print(has_folder_in_path.group(1))
+        _set_timestamp(has_folder_in_path.group(1))
+    else:
+        _set_timestamp(path) # pass in path as filename
 
