f6721d171fbbd09f1f589f5caa16945446fc746c
Sabrina Liao <sabrina.g.liao@gmail.com>
2017-01-09 22:46:29 -0800

    Add support for recursively searching for jpg files in folder. Finish basic last-modified timestamp 'fix' for pics from Jason.

--- settimestamp.py
+++ settimestamp.py
@@ -1,38 +1,27 @@
-import sys, os, platform, time, re
-from datetime import datetime
+import sys, os, platform, time, re, datetime
 
-def _get_creation_date(path_to_file):
-    """
-    Try to get the date that a file was created, falling back to when it was
-    last modified if that isn't possible.
-    See http://stackoverflow.com/a/39501288/1709587 for explanation.
-    """
-    if platform.system() == 'Windows':
-        return os.path.getctime(path_to_file)
-    else:
-        stat = os.stat(path_to_file)
-        try:
-            return stat.st_birthtime
-        except AttributeError:
-            # We're probably on Linux. No easy way to get creation dates here,
-            # so we'll settle for when its content was last modified.
-            return stat.st_mtime
-
-def _set_timestamp(filename):
+def _set_timestamp(filename, path):
     # check for file pattern
     jason_pattern= re.compile("(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})\.jpg") # jason's file pattern
     match = jason_pattern.match(filename)
     if match:
         # It's a pic by Jason
         # Turn components of date into epoch time to set on the last modified datetime field
-        # time.struct_time takes: year, month, day, hr, min, sec, weekday, day in year, is DST
-        struct = time.struct_time(tm_year=match.group(1), tm_mon=match.group(2), tm_mday=match.group(3),
-            tm_hour=match.group(4), tm_min=match.group(5), tm_sec=match.group(6), tm_isdst=0)
-        print(time.mktime(struct))
-    # creation_date = _get_creation_date(path) # not needed; just used to see what format the timestamp gets returned in
-    # print(time.time()) # to get now time
-    # print time.strftime("Before: %m/%d/%Y %I:%M:%S %p",time.localtime(os.stat(path).st_atime))
-    # os.utime(path, (os.stat(path).st_atime, creation_date))
+        # must convert strings to numbers for datetime
+        time_components = {
+            'year': int(match.group(1)),
+            'mon': int(match.group(2)),
+            'day': int(match.group(3)),
+            'hr': int(match.group(4)),
+            'min': int(match.group(5)),
+            'sec': int(match.group(6))
+        }
+        # datetime.datetime takes: year, month, day, hr, min, sec; timetuple() returns a struct_time to pass to time.mktime()
+        struct = datetime.datetime(time_components['year'], time_components['mon'], time_components['day'],
+            time_components['hr'], time_components['min'], time_components['sec']).timetuple()
+        print(time.strftime("Before: %m/%d/%Y %I:%M:%S %p", time.localtime(os.stat(path).st_mtime)))
+        os.utime(path, (os.stat(path).st_atime, time.mktime(struct)))
+        print(time.strftime("After: %m/%d/%Y %I:%M:%S %p", time.localtime(os.stat(path).st_mtime)))
     return
 
 # STARTS HERE
@@ -40,14 +29,18 @@ def _set_timestamp(filename):
 if len(sys.argv) <= 1:
     print('Need to pass argument')
 else:
-    # Retrieve file/filename from input/argument (TODO: also accept folder name eventually so can change multiple photos at once)
-    # ^-- read: https://docs.python.org/3/tutorial/inputoutput.html
-    # If folder, only grab .jpg(?) files (mainly want to avoid the CR2 files from echow)
     path = sys.argv[1]
-    has_folder_in_path = re.compile("^.*\/*.*\/(.*\..*)$").match(path)
-    if has_folder_in_path:
-        print(has_folder_in_path.group(1))
-        _set_timestamp(has_folder_in_path.group(1))
-    else:
-        _set_timestamp(path) # pass in path as filename
+    (dirname, filename) = os.path.split(os.path.expanduser(path))
+    print(dirname, filename)
+    if filename:
+        if os.path.isfile(path):
+            _set_timestamp(filename, path)
+    elif dirname and os.path.isdir(dirname):
+        # this is for when we pass a folder name for the argument (have to end w/ a '/' or else split thinks last part is filename)
+        # only grab .jpg(?) files (mainly want to avoid the CR2 files from echow)
+        for root, dirs, files in os.walk(dirname):
+            for file in files:
+                if file.endswith(".jpg"):
+                    # 'root' is the dirname path + whichever subfolder we might be walking through
+                    _set_timestamp(file, os.path.join(root, file))
 
